9fbfadd3cad6b82e93dbcddfbd81bf8468f7ea33
ryandzhunter <ryandz.hunter@gmail.com>
2014-09-29 05:21:34 +0700

    share dialog
    add share with Facebook dialog

--- src/id/ryandzhunter/testaryandi/ShareDialogActivity.java
+++ src/id/ryandzhunter/testaryandi/ShareDialogActivity.java
@@ -26,7 +26,7 @@ public class ShareDialogActivity extends Activity {
 
 		uiHelper = new UiLifecycleHelper(this, null);
 		uiHelper.onCreate(savedInstanceState);
-		publishFeedDialog();
+
 	}
 
 	@Override
@@ -35,6 +35,7 @@ public class ShareDialogActivity extends Activity {
 
 		uiHelper.onActivityResult(requestCode, resultCode, data,
 				new FacebookDialog.Callback() {
+			
 					@Override
 					public void onError(FacebookDialog.PendingCall pendingCall,
 							Exception error, Bundle data) {
@@ -54,18 +55,6 @@ public class ShareDialogActivity extends Activity {
 								.getNativeDialogPostId(data);
 					}
 				});
-
-		if (FacebookDialog.canPresentShareDialog(getApplicationContext(),
-				FacebookDialog.ShareDialogFeature.SHARE_DIALOG)) {
-			// Publish the post using the Share Dialog
-			FacebookDialog shareDialog = new FacebookDialog.ShareDialogBuilder(
-					this).setLink("https://developers.facebook.com/android")
-					.build();
-			uiHelper.trackPendingDialogCall(shareDialog.present());
-
-		} else {
-			// Fallback. For example, publish the post using the Feed Dialog
-		}
 
 	}
 
@@ -93,56 +82,74 @@ public class ShareDialogActivity extends Activity {
 		uiHelper.onDestroy();
 	}
 
-	private void shareWithDialogClick(View view) {
+	private void publishFeedDialog() {
+		Bundle params = new Bundle();
+		params.putString("name", "Facebook SDK for Android");
+		params.putString("caption",
+				"Build great social apps and get more installs.");
+		params.putString(
+				"description",
+				"The Facebook SDK for Android makes it easier and faster to develop Facebook integrated Android apps.");
+		params.putString("link", "https://developers.facebook.com/android");
+		params.putString("picture",
+				"https://raw.github.com/fbsamples/ios-3.x-howtos/master/Images/iossdk_logo.png");
 
+		WebDialog feedDialog = (new WebDialog.FeedDialogBuilder(this,
+				Session.getActiveSession(), params)).setOnCompleteListener(
+				new OnCompleteListener() {
+
+					@Override
+					public void onComplete(Bundle values,
+							FacebookException error) {
+						if (error == null) {
+							// When the story is posted, echo the success
+							// and the post Id.
+							final String postId = values.getString("post_id");
+							if (postId != null) {
+								Toast.makeText(getApplicationContext(),
+										"Posted story, id: " + postId,
+										Toast.LENGTH_SHORT).show();
+							} else {
+								// User clicked the Cancel button
+								Toast.makeText(
+										getApplicationContext()
+												.getApplicationContext(),
+										"Publish cancelled", Toast.LENGTH_SHORT)
+										.show();
+							}
+						} else if (error instanceof FacebookOperationCanceledException) {
+							// User clicked the "x" button
+							Toast.makeText(
+									getApplicationContext()
+											.getApplicationContext(),
+									"Publish cancelled", Toast.LENGTH_SHORT)
+									.show();
+						} else {
+							// Generic, ex: network error
+							Toast.makeText(
+									getApplicationContext()
+											.getApplicationContext(),
+									"Error posting story", Toast.LENGTH_SHORT)
+									.show();
+						}
+					}
+
+				}).build();
+		feedDialog.show();
 	}
 
-	private void publishFeedDialog() {
-	    Bundle params = new Bundle();
-	    params.putString("name", "Facebook SDK for Android");
-	    params.putString("caption", "Build great social apps and get more installs.");
-	    params.putString("description", "The Facebook SDK for Android makes it easier and faster to develop Facebook integrated Android apps.");
-	    params.putString("link", "https://developers.facebook.com/android");
-	    params.putString("picture", "https://raw.github.com/fbsamples/ios-3.x-howtos/master/Images/iossdk_logo.png");
+	public void shareWithDialogClick(View view) {
+		if (FacebookDialog.canPresentShareDialog(getApplicationContext(),
+				FacebookDialog.ShareDialogFeature.SHARE_DIALOG)) {
+				    // Publish the post using the Share Dialog
+				    FacebookDialog shareDialog = new FacebookDialog.ShareDialogBuilder(this)
+				            .setLink(null)
+				            .build();
+				    uiHelper.trackPendingDialogCall(shareDialog.present());
 
-	    WebDialog feedDialog = (
-	        new WebDialog.FeedDialogBuilder(this,
-	            Session.getActiveSession(),
-	            params))
-	        .setOnCompleteListener(new OnCompleteListener() {
-
-	            @Override
-	            public void onComplete(Bundle values,
-	                FacebookException error) {
-	                if (error == null) {
-	                    // When the story is posted, echo the success
-	                    // and the post Id.
-	                    final String postId = values.getString("post_id");
-	                    if (postId != null) {
-	                        Toast.makeText(getApplicationContext(),
-	                            "Posted story, id: "+postId,
-	                            Toast.LENGTH_SHORT).show();
-	                    } else {
-	                        // User clicked the Cancel button
-	                        Toast.makeText(getApplicationContext().getApplicationContext(), 
-	                            "Publish cancelled", 
-	                            Toast.LENGTH_SHORT).show();
-	                    }
-	                } else if (error instanceof FacebookOperationCanceledException) {
-	                    // User clicked the "x" button
-	                    Toast.makeText(getApplicationContext().getApplicationContext(), 
-	                        "Publish cancelled", 
-	                        Toast.LENGTH_SHORT).show();
-	                } else {
-	                    // Generic, ex: network error
-	                    Toast.makeText(getApplicationContext().getApplicationContext(), 
-	                        "Error posting story", 
-	                        Toast.LENGTH_SHORT).show();
-	                }
-	            }
-
-	        })
-	        .build();
-	    feedDialog.show();
+				} else {
+				    // Fallback. For example, publish the post using the Feed Dialog
+				   publishFeedDialog();
+				}
 	}
 }
